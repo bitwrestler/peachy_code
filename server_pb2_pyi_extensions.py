3ff54ff319015519041f61959a67eb488aaf7f3e
Eric <eric@spenc.org>
2024-04-10 18:15:47 -0400

    string DiffResult

--- server_pb2_pyi_extensions.py
+++ server_pb2_pyi_extensions.py
@@ -1,3 +1,4 @@
-from server_pb2 import PromptItem, PromptType , DiffRequest
+from server_pb2 import PromptItem, PromptType , DiffRequest, DiffResult
 PromptItem.__str__ = lambda self : f"{PromptType.Name(self.Type)} -> {self.Prompt.rstrip()}"
 DiffRequest.__str__ = lambda self :  "\n**BEGIN**" + "\n".join( map(lambda i: str(i), self.Request)) + "**END**\n"
+DiffResult.__str__ = lambda self : "".join(self.Result)
