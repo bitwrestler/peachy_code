4a59138681a9c2cedc87abd8c14cef8858468adf
Eric <eric@spenc.org>
2024-04-18 15:40:24 -0400

    server public Removefrom queue and logging

--- KnowledgeServerQueue.py
+++ KnowledgeServerQueue.py
@@ -1,5 +1,6 @@
 import threading
 import uuid
+import logging
 from server_pb2 import DiffRequest, DiffResult, ResponseType
 import server_pb2_pyi_extensions
 
@@ -14,8 +15,13 @@ class KnowledgeServerQueue:
     
     def isQueued(self, id : str) -> bool:
         return id in self.q
-    def removeQueued(self, id :str):
-        del self.q[id]
+    
+    def _removeQueued(self, id :str):
+        self.q.pop(id, None)
+
+    def RemoveQueued(self, id : str):
+        with self.lock:
+            self._removeQueued(id)
 
     @staticmethod
     def _newID() -> str:
@@ -35,17 +41,26 @@ class KnowledgeServerQueue:
     def TryQueue(self, request : DiffRequest):
         with self.lock:
             if request.IsStatusCheck():
+                logging.info("QUEUE: IsStatusCheck")
                 if self.isQueued(request.ResultID):
+                    logging.info("QUEUE: isQueued on IsStatusCheck")
                     if self.canRun():
-                        self.removeQueued(request.ResultID)
+                        logging.info("QUEUE: canRun on isQueued on IsStatusCheck")
+                        self._removeQueued(request.ResultID)
                         return (False, KnowledgeServerQueue.initResult(id=request.ResultID))
                     else:
+                        logging.info("QUEUE: not canRun on isQueued on IsStatusCheck")
                         e = self.q[request.ResultID]
                         return (True, KnowledgeServerQueue.initResult(id=request.ResultID, t = ResponseType.ResponseType_QUEUED))
                 else:
+                    logging.info("QUEUE: not id queued on IsStatusCheck")
                     return (False, KnowledgeServerQueue.initResult(id=request.ResultID))
             else:
+                logging.info("QUEUE: not IsStatusCheck")
                 if self.canRun():
-                    return(False, KnowledgeServerQueue.initResult())
+                    logging.info("QUEUE: canRun")
+                    tmpres = self.queueIt(request)
+                    return(False, KnowledgeServerQueue.initResult(id=tmpres.ResultID))
                 else:
+                    logging.info("QUEUE: not canRun -> queued")
                     return (True,self.queueIt(request))
